7245c8a6b4e8ea708962a334013cdd4535db276f
mw.rodriguez <mw.rodriguez@106-24.info2>
2025-09-19 12:27:38 +0200

    Exersice 1 1.0

--- src/exersice1/Exersice1.java
+++ src/exersice1/Exersice1.java
@@ -3,8 +3,8 @@ package exersice1;
 public class Exersice1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("Hello, how are you?");
+		System.out.println("Fine thanks");
 	}
 
 }
