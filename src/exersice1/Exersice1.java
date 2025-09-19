c209023ed82bee4def44971e31419ce1f2e5185d
mw.rodriguez <mw.rodriguez@106-24.info2>
2025-09-19 12:31:55 +0200

    dot after 2nd message

--- src/exersice1/Exersice1.java
+++ src/exersice1/Exersice1.java
@@ -4,7 +4,7 @@ public class Exersice1 {
 
 	public static void main(String[] args) {
 		System.out.print("Hello, how are you?");
-		System.out.println("Fine thanks");
+		System.out.println("Fine thanks.");
 	}
 
 }
